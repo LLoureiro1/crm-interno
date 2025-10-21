24c7c819cfc320f2f40ac256d16f45e12148151e
LLoureiro1 <97321918+LLoureiro1@users.noreply.github.com>
2025-10-21 17:03:52 -0300

    forçar cache

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// Desregistra qualquer Service Worker legado que possa interferir em cache
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations()
+    .then((regs) => {
+      regs.forEach((r) => r.unregister());
+    })
+    .catch(() => {});
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
